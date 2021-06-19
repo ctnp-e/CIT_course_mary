e3a6e9e1182d62aa20c6a9d0d93cf08ef96e4dba
masha <283ctnpexe@users.noreply.github.com>
2021-06-19 10:26:06 -0700

    hello...

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+"""
+    This is the hello world program in python
+"""
+
+import sys
+
+#print my version of python
+print()
+print("Version is: ",sys.version)
+print()
+
+#print hello world
+print("Hello World")
